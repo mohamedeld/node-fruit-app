c239bf3c81dc742123bccaff5168373893b538e6
mohamedeld <mohamed.azoz20010@gmail.com>
2023-04-21 13:41:00 +0200

    initalize commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+const replaceTemplate = require("./modules/replaceTemplate");
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`,'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`,'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`,'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req,res)=>{
+    const {query,pathname} = url.parse(req.url,true);
+
+    if(pathname ==="/" || pathname ==="/overview"){
+        res.writeHead(200,{'Content-type':'text/html'});
+        const cardsHtml = dataObj.map(el=> replaceTemplate(tempCard,el)).join('');
+        const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g,cardsHtml);
+        res.end(output);
+    }else if(pathname === "/product"){
+        res.writeHead(200,{'Content-type':'text/html'});
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct,product);
+        res.end(output);
+    }else if(pathname === "/api"){
+        res.writeHead(200,{'Content-type':'application/json'});
+        res.end(data);
+    }else{
+        res.writeHead(404,{
+            'Content-type':'text/html'
+        })
+        res.end("<h1>not found</h1>");
+    }
+});
+
+server.listen(8000,()=>{
+    console.log("listening");
+})
