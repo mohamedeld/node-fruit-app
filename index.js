255a62e79ae71951bb0bc93ccd9822c14fede640
mohamedeld <mohamed.azoz20010@gmail.com>
2023-04-21 13:57:50 +0200

    add packages

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
-
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
+
 
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`,'utf-8');
@@ -11,6 +12,8 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`,'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el= slugify(el.productName,{lower:true}))
 
 const server = http.createServer((req,res)=>{
     const {query,pathname} = url.parse(req.url,true);
